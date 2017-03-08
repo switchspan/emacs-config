f7d3e343ed0e0111a34777576356e685dbadb55c
Ken Taylor <taylor.kenneth@gmail.com>
2017-03-08 08:25:57 -0500

    update packages init to work cross platform and autoload packages

--- init_packages.el
+++ init_packages.el
@@ -2,17 +2,27 @@
 ;;(load "package")
 (require 'package)
 
+;; requires emacs > 24
 (message "Loading package archives")
 
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+; list the packages you want
+(setq package-list '(dash magit kooten-theme))
 
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+; list the repositories containing them
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (setq package-archive-enable-alist '(("melpa" deft magit)))
 
+; activate all the packages (in particular autoloads)
 (package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
