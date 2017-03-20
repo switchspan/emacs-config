4c4628616a69997fd640951fceaed7ebc4a35759
Ken Taylor <ktaylor@gotechark.com>
2017-03-20 10:31:21 -0400

    add missing package repos to package load

--- init_packages.el
+++ init_packages.el
@@ -2,11 +2,15 @@
 ;; this was adopted from emacs-starter-kit (https://github.com/technomancy/emacs-starter-kit)
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 
+;; define my required packages
 (defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous
                                       find-file-in-project magit smex scpaste dash 
                                       git-gutter kooten-theme))
 
+;; load any missing packages
 (package-initialize)
 (dolist (p my-packages)
   (when (not (package-installed-p p))
