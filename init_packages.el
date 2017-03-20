1c886b3b968253d6f05f5d800d25bb432f3be160
Ken Taylor <ktaylor@gotechark.com>
2017-03-20 10:26:25 -0400

    updated to use package loading from emacs-start-kit

--- init_packages.el
+++ init_packages.el
@@ -1,35 +1,13 @@
-;; load the package archives
-;;(load "package")
-
-(message "Loading package archives")
+;; load the needed packages
+;; this was adopted from emacs-starter-kit (https://github.com/technomancy/emacs-starter-kit)
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+
+(defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous
+                                      find-file-in-project magit smex scpaste dash 
+                                      git-gutter kooten-theme))
+
 (package-initialize)
-
-(package-refresh-contents t)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
-
-;; packages to load
-(use-package dash
-  :ensure t)
-
-(use-package magit
-  :ensure t)
-
-(use-package git-gutter
-  :ensure t)
-
-(use-package kooten-theme)
-
-(message "Done loading packages!")
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
